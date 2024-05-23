2ab69c566ed164f36af151d54a2d5d83a37551a0
rash-27 <rashmik2705@gmail.com>
2024-05-23 14:42:49 +0530

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Background from "./components/Background";
+import Notfound from "./pages/Notfound";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/*" element={<Notfound />} />
     </Routes>
   </BrowserRouter>
   )
---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,11 @@
+function Notfound() {
+    return (
+    <div className="text-center flex h-screen justify-center"> 
+        <div className="flex flex-col justify-center">
+            <div className="text-5xl text-white font-heading">Error 404 - Page Not found</div>
+        </div>
+     </div>
+    )
+  }
+  
+  export default Notfound
